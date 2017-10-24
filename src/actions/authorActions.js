61940740738e12b862ceaedac613a5f78b07ca9d
Luke Baker <akabaker@gmail.com>
2017-10-24 13:14:00 -0500

    Added firs taction

---
+++ src/actions/authorActions.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var AuthorApi = require('../api/authorApi');
+var ActionTypes = require('../constants/actionTypes');
+
+var AuthorActions = {
+    createAuthor: function(author) {
+        var newAuthor = AuthorApi.saveAuthor(author);
+
+        // Use dispatcher to tell all stores that an author was created
+        Dispatcher.dispatch({
+            actionType: ActionTypes.CREATE_AUTHOR,
+            author: newAuthor
+        });
+    }
+};
+
+module.exports = AuthorActions
