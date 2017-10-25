cefa8879291fdef78eba0050ed2f996733124765
Luke Baker <akabaker@gmail.com>
2017-10-24 22:52:49 -0500

    use authorstore instead of directly hitting the authorapi

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -1,7 +1,8 @@
 "use strict";
 
 var React = require('react');
-var AuthorApi = require('../../api/authorApi');
+var AuthorStore = require('../../stores/authorStore');
+var AuthorActions = require('../../actions/authorActions');
 var AuthorList = require('./authorList');
 var Router = require('react-router');
 var Link = require('react-router').Link;
@@ -9,14 +10,8 @@ var Link = require('react-router').Link;
 var AuthorPage = React.createClass({
     getInitialState: function() {
         return {
-            authors: []
+            authors: AuthorStore.getAllAuthors()
         };
-    },
-
-    componentDidMount: function() {
-        if (this.isMounted()) {
-            this.setState({authors: AuthorApi.getAllAuthors()});
-        }
     },
 
     render: function() {
